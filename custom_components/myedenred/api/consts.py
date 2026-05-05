3dd36e5ff4a1098c3e94757c54e57d975724365a
Rui Baptista <98954176+rbaptista1995@users.noreply.github.com>
2026-05-05 09:00:19 +0100

    Adapt integration to new Pluxee consumer portal flow

--- custom_components/myedenred/api/consts.py
+++ custom_components/myedenred/api/consts.py
@@ -1,3 +1,2 @@
-API_LOGIN_URL = "https://consumidores.pluxee.pt/edenred-customer/api/authenticate/default"
-API_LIST_URL = "https://consumidores.pluxee.pt/edenred-customer/api/protected/card/list"
-API_ACCOUNTMOVEMENT_URL = "https://consumidores.pluxee.pt/edenred-customer/api/protected/card/{}/accountmovement"
+API_LOGIN_URL = "https://portal.admin.pluxee.pt/login_processing.php"
+API_DASHBOARD_URL = "https://consumidores.pluxee.pt/"
